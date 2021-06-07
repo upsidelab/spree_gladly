5d97aeb976b1c51dcf74327f44761e7d0a8cb20a
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2021-06-07 13:00:14 +0200

    Add comments to initializer template

--- lib/generators/spree_gladly/install/templates/config/initializers/spree_gladly.rb
+++ lib/generators/spree_gladly/install/templates/config/initializers/spree_gladly.rb
@@ -1,6 +1,12 @@
 SpreeGladly.setup do |config|
-  # TODO: add example config and comments
+  # The key used to validate the Gladly lookup request signature.
+  # We recommend using a secure random string of length over 32.
+  # Setting this value to `nil` disables request signature validation.
+  # Default is `nil`.
+  # config.signing_key = '<%= SecureRandom.base64(32) %>'
 
-  # config.signing_key = nil
-  # config.signing_threshold = nil
+  # The request's timestamp is validated against `signing_threshold` to prevent replay attacks.
+  # Setting this value to `nil` disables the threshold validation.
+  # Default is `nil`.
+  # config.signing_threshold = 5.minutes
 end
