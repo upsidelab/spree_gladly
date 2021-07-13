70d4d33f7581e1cff3ed31ddc13a80e5be41a1bd
przemosk <p.kosakowski@gmail.com>
2021-07-13 09:35:19 +0200

    Add more specs

--- spec/presenters/customer/basic_lookup_presenter_spec.rb
+++ spec/presenters/customer/basic_lookup_presenter_spec.rb
@@ -1,7 +1,56 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Customer::BasicLookupPresenter, as: :presenter do
   subject { described_class.new(resource: resource) }
 
-  describe '#to_h', -> { true }
+  describe '#to_h' do
+    context 'only guest customers' do
+      let!(:order) { create(:completed_order_with_pending_payment, user_id: nil, email: 'guest@example.com') }
+      let!(:order1) { create(:completed_order_with_pending_payment, user_id: nil, email: 'guest1@example.com') }
+      let!(:guest_customers) { [order, order1] }
+      let(:resource) { OpenStruct.new(guest_customers: guest_customers, registered_customers: []) }
+
+      it 'return formatted payload' do
+        result = subject.to_h
+
+        expect(result.empty?).to eq false
+        expect(result.first.keys).to eq %i[externalCustomerId address name emails phones]
+      end
+    end
+
+    context 'only registered customers' do
+      let!(:customer) { create(:user_with_addresses) }
+      let!(:customer1) { create(:user_with_addresses) }
+      let!(:registered_customers) { [customer, customer1] }
+      let(:resource) { OpenStruct.new(guest_customers: [], registered_customers: registered_customers) }
+
+      it 'return formatted payload' do
+        result = subject.to_h
+
+        expect(result.empty?).to eq false
+        expect(result.first.keys).to eq %i[externalCustomerId spreeId address name emails phones]
+      end
+    end
+
+    context 'registered and guest customers' do
+      let!(:order) { create(:completed_order_with_pending_payment, user_id: nil, email: 'guest@example.com') }
+      let!(:order1) { create(:completed_order_with_pending_payment, user_id: nil, email: 'guest1@example.com') }
+      let!(:customer) { create(:user_with_addresses) }
+      let!(:customer1) { create(:user_with_addresses) }
+      let!(:guest_customers) { [order, order1] }
+      let!(:registered_customers) { [customer, customer1] }
+      let(:resource) { OpenStruct.new(guest_customers: guest_customers, registered_customers: registered_customers) }
+
+      it 'return formatted payload' do
+        result = subject.to_h
+        registered_format = result.find { |i| i[:externalCustomerId] == customer.email }.keys
+        guest_format = result.find { |i| i[:externalCustomerId] == order.email }.keys
+
+        expect(registered_format).to eq %i[externalCustomerId spreeId address name emails phones]
+        expect(guest_format).to eq %i[externalCustomerId address name emails phones]
+      end
+    end
+  end
 end
--- spec/presenters/customer/detailed_lookup_presenter_spec.rb
+++ spec/presenters/customer/detailed_lookup_presenter_spec.rb
@@ -1,7 +1,47 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Customer::DetailedLookupPresenter, as: :presenter do
   subject { described_class.new(resource: resource) }
 
-  describe '#to_h', -> { true }
+  describe '#to_h' do
+    context 'registered customer' do
+      let!(:customer) { create(:user_with_addresses) }
+      let!(:transactions) { create_list(:completed_order_with_pending_payment, 2) }
+      let(:resource) { OpenStruct.new(customer: customer, transactions: transactions, guest: false) }
+
+      it 'return formatted payload' do
+        result = subject.to_h
+
+        expect(result.first.keys).to eq %i[externalCustomerId name address emails phones customAttributes transactions]
+        expect(result.empty?).to eq false
+      end
+    end
+
+    context 'guest customer' do
+      let!(:customer) { transactions.first }
+      # rubocop:disable Layout/LineLength
+      let!(:transactions) { create_list(:completed_order_with_pending_payment, 3, user_id: nil, email: 'guest@example.com') }
+      # rubocop:enable Layout/LineLength
+      let(:resource) { OpenStruct.new(customer: customer, transactions: transactions, guest: true) }
+
+      it 'return formatted payload' do
+        result = subject.to_h
+
+        expect(result.first.keys).to eq %i[externalCustomerId customAttributes transactions]
+        expect(result.empty?).to eq false
+      end
+    end
+
+    context 'given no customer' do
+      let(:resource) { OpenStruct.new(customer: [], transactions: []) }
+
+      it 'return empty array' do
+        result = subject.to_h
+
+        expect(result).to eq []
+      end
+    end
+  end
 end
